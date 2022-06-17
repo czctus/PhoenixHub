0785ef051aff6da9b332a2695ea605a80abd4886
Czctus <71540581+czctus@users.noreply.github.com>
2022-06-17 14:01:44 +0000

    Created A New Script
    Idk man

---
+++ gfn.js
@@ -0,0 +1,9 @@
+javascript:
+var url = prompt("Welcome, use HTTPS:// ty. Visit global-quest.dev for more bookmarks, use about:blank btw!", "https://"); /*what it says.*/
+var ver = "V1"; /*version*/
+var prom = "The site " + url + " has loaded. (hopefully it worked.) Check out my website global-quest.dev!"; /*showing it opened.*/
+if (url != null) { (function () {var iframe = document.createElement('iframe'); /*create iframe*/
+iframe.src="https://w.epicedufinder.org/main/"+url; /* the url entered */
+ iframe.width = window.innerWidth;
+  iframe.height = window.innerHeight;
+   document.body.appendChild(iframe);})();};
